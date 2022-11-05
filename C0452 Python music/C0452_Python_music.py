769c6f706e56598cda1d5674178cedfbbcd18ceb
Robert Hensher <robertjh2016@gmail.com>
2022-11-05 18:58:27 +0000

    Add project files.

---
+++ C0452 Python music/C0452_Python_music.py
@@ -0,0 +1,11 @@
+## Music
+## This shows artists, songs, number of plays. Implemented in nested dictionaries. Format example - key1: (key0:value0,key1:value1...)
+music = {
+        'Bob Dylan':{'end of the line':1013213, 'the boxer':654124, 'hurricane':52355205}, 
+        'Madonna':{'hung up':1443176, 'frozen':3567406, 'material girl':145325},
+        'MF DOOM':{'rap snitches':1412223, 'Sofa king':37638, 'doomsday':1543535},
+        'Freddie Dredd':{'GTG':244534, 'WTH':543336, 'cha cha':1234148}
+        }
+
+print(music)
+
